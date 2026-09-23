8894b75cca4be777e8ac8885d51310facda6aef0
Shaswat Raj <shaswatraj3@gmail.com>
2026-09-23 21:50:46 +0530

    fix: use current Excalidraw API prop

--- src/components/excalidraw-client.tsx
+++ src/components/excalidraw-client.tsx
@@ -148,7 +148,7 @@ export default function ExcalidrawClient({
         <Excalidraw
           theme={dark ? "dark" : "light"}
           initialData={initialData}
-          onExcalidrawAPI={setApi}
+          excalidrawAPI={setApi}
           UIOptions={{
             canvasActions: {
               saveToActiveFile: false,
